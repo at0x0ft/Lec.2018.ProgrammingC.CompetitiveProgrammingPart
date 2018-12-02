b168e77a864a94f272ddf9a96611e60746adb120
at0x0ft <at0p0ft@outlook.com>
2018-12-02 22:14:02 +0900

    Solved problem 23.

--- 9/23.cpp
+++ 9/23.cpp
@@ -2,30 +2,60 @@
 
 using namespace std;
 
-void cReadLn(int &n)
+#define A_MAX 10000
+
+void countingSort(const vector<int> &a, vector<int> &b, const int k)
 {
-    scanf("%d", &n);
+    vector<int> c(k + 1);
+    for (int i = 0; i <= k; i++)
+    {
+        c[i] = 0;
+    }
+
+    int n = a.size();
+    for (int j = 0; j < n; j++)
+        c[a[j]]++;
+
+    // Printing counting_array.
+    int sum = 0;
+    for (int i = 0; sum < n; i++)
+    {
+        sum += c[i];
+        printf("%d", c[i]);
+        if (sum < n)
+            printf(" ");
+    }
+    printf("\n");
+    // END printing counting_array.
+
+    for (int i = 1; i <= k; i++)
+        c[i] += c[i - 1];
+
+    for (int j = n - 1; j >= 0; j--)
+    {
+        b[n - 1 - (c[a[j]] - 1)] = a[j];
+        c[a[j]]--;
+    }
 }
 
 int main()
 {
     int n;
-    cReadLn(n);
-    set<string> dict;
-
+    scanf("%d", &n);
+    vector<int> a(n), b(n);
     for (int i = 0; i < n; i++)
     {
-        string cmd;
-        cin >> cmd;
-        string val;
-        cin >> val;
-        if (cmd == "add")
-            dict.insert(val);
-        else if (cmd == "remove")
-            dict.erase(val);
-        else if (cmd == "find")
-            dict.count(val) > 0 ? printf("yes\n") : printf("no\n");
+        scanf("%d", &a[i]);
     }
+
+    countingSort(a, b, A_MAX);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", b[i]);
+        if (i != n - 1)
+            printf(" ");
+    }
+    printf("\n");
 
     return 0;
 }
