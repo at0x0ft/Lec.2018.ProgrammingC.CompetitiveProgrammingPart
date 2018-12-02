cbe614d77c51987d4a1e8bc7797543edaf725e1b
at0x0ft <at0p0ft@outlook.com>
2018-12-02 17:39:54 +0900

    Solved problem 16.

--- 9/16.cpp
+++ 9/16.cpp
@@ -2,155 +2,28 @@
 
 using namespace std;
 
-#define QUEUE_SIZE 500
-
-/* Dual LinkedList Definition */
-
-typedef struct Node
+long long int combi(const int n, const int r)
 {
-    int data;
-    Node *next;
-    Node *prev;
-} Node;
+    if (n - r < r)
+        return combi(n, n - r);
 
-Node *head = NULL;
-Node *tail = NULL;
-
-void listAddNode(Node *prevNode, int val)
-{
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    Node *nextNode = prevNode->next;
-    prevNode->next = newNode;
-    newNode->next = nextNode;
-    nextNode->prev = newNode;
-    newNode->prev = prevNode;
-
-    newNode->data = val;
-}
-
-void listDelNode(Node *p)
-{
-    p->prev->next = p->next;
-    p->next->prev = p->prev;
-    free(p);
-}
-
-void listInit()
-{
-    head = (Node *)malloc(sizeof(Node));
-    tail = (Node *)malloc(sizeof(Node));
-    head->next = tail;
-    head->prev = NULL;
-    head->data = INT_MAX;
-    tail->next = NULL;
-    tail->prev = head;
-    tail->data = INT_MIN;
-}
-
-void listAddHead(int val)
-{
-    listAddNode(head, val);
-}
-
-void listAddTail(int val)
-{
-    listAddNode(tail->prev, val);
-}
-
-void listRemove(int val)
-{
-    for (Node *p = head->next; p != tail;)
+    long long int res = 1;
+    for (int i = 1; i <= r; i++)
     {
-        if (p->data == val)
-        {
-            listDelNode(p);
-            return;
-        }
+        res *= (n - i + 1);
+        res /= i;
     }
-}
-
-void listDelHead()
-{
-    listDelNode(head->next);
-}
-
-void listDelTail()
-{
-    listDelNode(tail->prev);
-    tail->prev = tail->prev;
-}
-
-void listDelAll()
-{
-    for (Node *p = head->next; p != tail;)
-    {
-        p = p->next;
-        listDelNode(p->prev);
-    }
-}
-
-void listFin()
-{
-    listDelAll();
-    free(head);
-    free(tail);
-}
-
-void printListLn()
-{
-    for (Node *p = head->next; p != tail; p = p->next)
-    {
-        cout << p->data;
-        if (p->next != tail)
-            printf(" ");
-    }
-    printf("\n");
-}
-
-/* END Dual LinkedList Definition */
-
-void cReadLn(int &n)
-{
-    scanf("%d", &n);
+    return res;
 }
 
 int main()
 {
+    int m;
+    scanf("%d", &m);
     int n;
-    cReadLn(n);
-    listInit();
+    scanf("%d", &n);
 
-    int val;
-    for (int i = 0; i < n; i++)
-    {
-        string cmd;
-        cin >> cmd;
-        if (cmd == "insertFirst")
-        {
-            scanf("%d", &val);
-            listAddHead(val);
-        }
-        else if (cmd == "insertLast")
-        {
-            scanf("%d", &val);
-            listAddTail(val);
-        }
-        else if (cmd == "delete")
-        {
-            scanf("%d", &val);
-            listRemove(val);
-        }
-        else if (cmd == "deleteFirst")
-            listDelHead();
-        else if (cmd == "deleteLast")
-            listDelTail();
-        else if (cmd == "deleteAll")
-            listDelAll();
-    }
-
-    printListLn();
-
-    listFin();
+    printf("%lld\n", combi(m - 1, m - n));
 
     return 0;
 }
