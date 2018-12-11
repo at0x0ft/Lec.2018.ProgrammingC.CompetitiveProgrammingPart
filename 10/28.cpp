57f84f934f32fbd324a4f4d1a18fe19c84d42719
at0x0ft <at0p0ft@outlook.com>
2018-12-11 22:21:53 +0900

    Solved 28.cpp

--- 10/28.cpp
+++ 10/28.cpp
@@ -5,11 +5,33 @@ using namespace std;
 struct node
 {
     int parent;
-    int left;
-    int right;
     int sibling;
+    int left, right;
+    int chCnt;
     int depth;
+    int height;
 } Node;
+
+int adjustDepth(vector<node> &v, int id, const int depth)
+{
+    v[id].depth = depth;
+    int len = v[id].chCnt;
+
+    int lh = -1, rh = -1;
+    if (v[id].left >= 0)
+        lh = adjustDepth(v, v[id].left, depth + 1);
+    if (v[id].right >= 0)
+        rh = adjustDepth(v, v[id].right, depth + 1);
+
+    v[id].height = max(lh, rh) + 1;
+
+    return v[id].height;
+}
+
+int findRootId(const vector<node> &t, const int id)
+{
+    return t[id].parent < 0 ? id : findRootId(t, id + 1);
+}
 
 int main()
 {
@@ -19,31 +41,44 @@ int main()
     for (int i = 0; i < n; i++)
     {
         t[i].parent = -1;
-        t[i].depth = 0;
+        t[i].sibling = -1;
+        t[i].left = -1;
+        t[i].right = -1;
+        t[i].chCnt = 0;
     }
 
     for (int i = 0; i < n; i++)
     {
         int id, chN;
-        scanf("%d %d", &id, &chN);
+        scanf("%d", &id);
+        scanf("%d %d", &(t[id].left), &(t[id].right));
 
-        for (int j = 0; j < chN; j++)
+        if (t[id].left >= 0 || t[id].right >= 0)
         {
-            int ch;
-            scanf("%d", &ch);
+            t[t[id].left].parent = id;
+            t[t[id].right].parent = id;
+            t[t[id].left].sibling = t[id].right;
+            t[t[id].right].sibling = t[id].left;
 
-            t[ch].parent = id;
-            t[ch].depth = t[id].depth + 1;
+            if (t[id].left >= 0 && t[id].right >= 0)
+                t[id].chCnt = 2;
+            else
+                t[id].chCnt = 1;
         }
     }
 
+    adjustDepth(t, findRootId(t, 0), 0);
+
     for (int i = 0; i < n; i++)
     {
-        printf("%d %d %d", i, t[i].parent, t[i].depth);
-        if (i != n - 1)
-            printf("\n");
+        printf("%d %d %d %d %d %d\n",
+               i,
+               t[i].parent,
+               t[i].sibling,
+               t[i].chCnt,
+               t[i].depth,
+               t[i].height);
     }
-    printf("\n");
 
     return 0;
 }
