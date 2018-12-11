13abce44d883de99b7dce4f0b2dffb43b56435dd
at0x0ft <at0p0ft@outlook.com>
2018-12-11 15:03:06 +0900

    WIP 29.cpp.

--- 10/29.cpp
+++ 10/29.cpp
@@ -2,28 +2,73 @@
 
 using namespace std;
 
-long long int combi(const int n, const int r)
+struct Node
 {
-    if (n - r < r)
-        return combi(n, n - r);
+    int parent, left, right;
+} node;
 
-    long long int res = 1;
-    for (int i = 1; i <= r; i++)
-    {
-        res *= (n - i + 1);
-        res /= i;
-    }
-    return res;
+void preorder(int id, vector<Node> &t)
+{
+    printf(" %d", id);
+
+    if (t[id].left != -1)
+        preorder(t[id].left, t);
+
+    if (t[id].right != -1)
+        preorder(t[id].right, t);
+}
+
+void inorder(int id, vector<Node> &t)
+{
+    if (t[id].left != -1)
+        inorder(t[id].left, t);
+
+    printf(" %d", id);
+
+    if (t[id].right != -1)
+        inorder(t[id].right, t);
+}
+
+void postorder(int id, vector<Node> &t)
+{
+    if (t[id].left != -1)
+        postorder(t[id].left, t);
+    if (t[id].right != -1)
+        postorder(t[id].right, t);
+
+    printf(" %d", id);
 }
 
 int main()
 {
-    int m;
-    scanf("%d", &m);
     int n;
     scanf("%d", &n);
+    vector<Node> t(n);
+    for (int i = 0; i < n; i++)
+    {
+        t[i].parent = -1;
+        t[i].left = -1;
+        t[i].right = -1;
+    }
 
-    printf("%lld\n", combi(m - 1, m - n));
+    for (int i = 0; i < n; i++)
+    {
+        int id;
+        scanf("%d", &id);
+        scanf("%d %d", &t[id].left, &t[id].right);
+    }
+
+    printf("Preorder\n");
+    preorder(0, t);
+    printf("\n");
+
+    printf("Inorder\n");
+    inorder(0, t);
+    printf("\n");
+
+    printf("Postorder\n");
+    postorder(0, t);
+    printf("\n");
 
     return 0;
 }
