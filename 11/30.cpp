bd390f2d5d410aeefeaba2ea0d839e0a095520e0
at0x0ft <at0p0ft@outlook.com>
2019-01-08 09:45:02 +0900

    Solved 30.cpp.

--- 11/30.cpp
+++ 11/30.cpp
@@ -2,131 +2,97 @@
 
 using namespace std;
 
-#define TREE_NUM 512
-#define KEY_DEFAULT_VAL -1
-
 struct Node
 {
-    int key, parent, left, right;
+    int key;
+    Node *right, *left, *parent;
 };
 
-Node T[TREE_NUM];
-int nodeNum = 0;
+Node *root, *nullNode;
 
-void preorder(int id)
+Node *createNode(int key)
 {
-    printf(" %d", T[id]);
-
-    if (t[id].left != -1)
-        preorder(t[id].left);
-
-    if (t[id].right != -1)
-        preorder(t[id].right);
+    Node *n = (Node *)malloc(sizeof(Node));
+    n->key = key;
+    n->left = nullNode;
+    n->right = nullNode;
+    return n;
 }
 
-void inorder(int id)
+void insert(int k)
 {
-    if (t[id].left != -1)
-        inorder(t[id].left);
-
-    printf(" %d", id);
-
-    if (t[id].right != -1)
-        inorder(t[id].right);
-}
-
-Node createNode(int key)
-{
-    T[nodeNum].key = key;
-    T[nodeNum].parent = -1;
-    T[nodeNum].left = -1;
-    T[nodeNum].right = -1;
-    nodeNum++;
-    return T[nodeNum];
-}
-
-void treeInit()
-{
-    for (int i = 0; i < TREE_NUM; i++)
+    Node *y = root, *z = createNode(k);
+    for (Node *i = root; i != nullNode; z->key < i->key ? i = i->left : i = i->right)
     {
-        T[i].key = KEY_DEFAULT_VAL;
-        T[i].parent = -1;
-        T[i].left = -1;
-        T[i].right = -1;
+        y = i;
     }
+
+    z->parent = y;
+    if (y == nullNode)
+        root = z;
+    else if (z->key < y->key)
+        y->left = z;
+    else
+        y->right = z;
 }
 
-bool isNullNode(Node n)
+void inorder(Node *n)
 {
-    return n.key == KEY_DEFAULT_VAL;
+    if (n == nullNode)
+        return;
+    inorder(n->left);
+    printf(" %d", n->key);
+    inorder(n->right);
 }
 
-void insert(Node *z)
+void preorder(Node *n)
 {
-    Node *p = NULL, *x = T;
-    while (isNullNode(*x))
-    {
-        p = x;
-        if (z->key < x->key)
-            x = x->left;
-        else
-            x = x->right;
-        z->parent = p;
-        if (p == NULL)
-            T[0] = *z;
-        else if (z->key < p->key)
-            p->left = z;
-        else
-            p->right = z;
-    }
-    nodeNum++;
+    if (n == nullNode)
+        return;
+    printf(" %d", n->key);
+    preorder(n->left);
+    preorder(n->right);
+}
+
+void deleteNode(Node *n)
+{
+    if (n == nullNode)
+        return;
+    deleteNode(n->left);
+    deleteNode(n->right);
+    free(n);
+}
+
+void deleteTree()
+{
+    deleteNode(root);
 }
 
 int main()
 {
     int n;
     scanf("%d", &n);
-    vector<int> v(n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &v[i]);
-    }
-
-    int cnt = 0;
-    bool flg = true;
-    cnt = 0;
-    while (flg)
-    {
-        flg = false;
-        for (int i = n - 1; i >= 1; i--)
-        {
-            if (v[i] > v[i - 1])
-            {
-                swap(v[i], v[i - 1]);
-                flg = true;
-                cnt++;
-            }
-        }
-    }
-
-    int n;
-    scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
         string ord;
         cin >> ord;
-        if (ord == "print")
-            printf("%d\n", extract(a));
-        else // insert
+        if (ord == "insert")
         {
-            int val;
-            cin >> val;
-            Node n = createNode(val);
-            insert(&n);
+            int k;
+            cin >> k;
+            insert(k);
+        }
+        else
+        {
+            inorder(root);
+            printf("\n");
+
+            preorder(root);
+            printf("\n");
         }
     }
 
-    printf("%d\n", cnt);
+    deleteTree();
 
     return 0;
 }
