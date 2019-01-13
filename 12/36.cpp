bafac5a6dc52262c85ce1d0a8508b4cb39e08905
at0x0ft <at0p0ft@outlook.com>
2019-01-14 02:23:54 +0900

    Solved 36.cpp.

--- 12/36.cpp
+++ 12/36.cpp
@@ -2,28 +2,34 @@
 
 using namespace std;
 
+#define MAX_MN 20
+
+pair<bool, long long int> dp[MAX_MN + 1][MAX_MN + 1];
+
 long long int combi(const int n, const int r)
 {
-    if (n - r < r)
+    if (dp[n][r].first)
+        return dp[n][r].second;
+
+    if (r == 0)
+    {
+        dp[n][r].first = true;
+        dp[n][r].second = 1;
+        return 1;
+    }
+    if (r > n - r)
         return combi(n, n - r);
 
-    long long int res = 1;
-    for (int i = 1; i <= r; i++)
-    {
-        res *= (n - i + 1);
-        res /= i;
-    }
-    return res;
+    dp[n][r].first = true;
+    dp[n][r].second = combi(n - 1, r - 1) + combi(n - 1, r);
+    return dp[n][r].second;
 }
 
 int main()
 {
-    int m;
-    scanf("%d", &m);
-    int n;
-    scanf("%d", &n);
-
-    printf("%lld\n", combi(m - 1, m - n));
+    int m, n;
+    scanf("%d %d", &m, &n);
+    printf("%lld\n", combi(m + n - 1, m));
 
     return 0;
 }
