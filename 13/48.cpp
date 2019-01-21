11a1b6bd7dd87d1a5f0f42679fb604d30503b83d
at0x0ft <at0p0ft@outlook.com>
2019-01-22 08:20:25 +0900

    Solved problem 48.

--- 13/48.cpp
+++ 13/48.cpp
@@ -2,29 +2,56 @@
 
 using namespace std;
 
-int main()
+class UnionFind
 {
-    int n;
-    scanf("%d", &n);
-    vector<int> dsts(n);
-    for (int i = 0; i < n; i++)
-    {
-        int cnt = 0;
-        for (int j = 0; j < n; j++)
-        {
-            int buf;
-            scanf("%d", &buf);
-            if (buf != 0)
-                dsts[cnt++] = j;
-        }
+  private:
+    vector<int> _parent;
 
-        printf("%d %d", i + 1, cnt);
-        for (int j = 0; j < cnt; j++)
+  public:
+    UnionFind(const int n) : _parent(n)
+    {
+        for (int i = 0; i < n; i++)
         {
-            printf(" %d", dsts[j] + 1);
+            _parent[i] = i;
         }
-        printf("\n");
     }
 
-    return 0;
+    int root(const int v)
+    {
+        if (_parent[v] != v)
+            _parent[v] = root(_parent[v]);
+        return _parent[v];
+    }
+
+    void unite(const int v1, const int v2)
+    {
+        int r1 = root(v1);
+        int r2 = root(v2);
+        if (r1 == r2)
+            return;
+        _parent[r2] = r1;
+    }
+
+    bool same(const int v1, const int v2)
+    {
+        return root(v1) == root(v2);
+    }
+};
+
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    UnionFind uf(n);
+    for (int i = 0; i < m; i++)
+    {
+        int v1, v2;
+        scanf("%d %d", &v1, &v2);
+        uf.unite(v1, v2);
+    }
+
+    int v1, v2;
+    scanf("%d %d", &v1, &v2);
+    printf("%s\n", uf.same(v1, v2) ? "true" : "false");
 }
