f0284bfde6c2c677b084dc641aa4ece705feb68e
at0x0ft <at0p0ft@outlook.com>
2019-01-23 03:36:26 +0900

    Solved problem 51.

--- 14/51.cpp
+++ 14/51.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 pair<double, double> calc(const pair<int, int> ps[P_NUM], const pair<int, int> &sp)
 {
-    int c1 = ps[0].first - ps[1].first, c2 = ps[1].second - ps[0].second;
+    int c1 = ps[0].first - ps[1].first, c2 = ps[0].second - ps[1].second;
 
     return pair<double, double>(
-        (1.0 * (sp.first * c1 * c1 + 2 * ps[0].first * c2 * c2 + 2 * ps[0].second * c1 * c2 - sp.second * c1 * c2)) / (c1 * c1 + c2 * c2),
-        (1.0 * (2 * ps[0].first * c1 * c2 + 2 * ps[0].second * c1 * c1 - sp.first * c1 * c2 + sp.second * c2 * c2)) / (c1 * c1 + c2 * c2));
+        (1.0 * (c1 * c1 * sp.first + 2 * c1 * c2 * (sp.second - ps[0].second) + c2 * c2 * (2 * ps[0].first - sp.first))) / (c1 * c1 + c2 * c2),
+        (1.0 * (c1 * c1 * (2 * ps[0].second - sp.second) + 2 * c1 * c2 * (sp.first - ps[0].first) + c2 * c2 * sp.second)) / (c1 * c1 + c2 * c2));
 }
 
 int main()
